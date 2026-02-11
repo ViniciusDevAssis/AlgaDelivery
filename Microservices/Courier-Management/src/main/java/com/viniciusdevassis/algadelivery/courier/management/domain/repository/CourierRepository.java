7677da49388bd6afcee42ebaed9bcea889f5bab8
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2026-02-10 21:45:00 -0300

    Domain Events, Event Driven Architecture e Kafka

--- Microservices/Courier-Management/src/main/java/com/viniciusdevassis/algadelivery/courier/management/domain/repository/CourierRepository.java
+++ Microservices/Courier-Management/src/main/java/com/viniciusdevassis/algadelivery/courier/management/domain/repository/CourierRepository.java
@@ -4,8 +4,13 @@ import com.viniciusdevassis.algadelivery.courier.management.domain.model.Courier
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CourierRepository extends JpaRepository<Courier, UUID> {
+
+    Optional<Courier> findTop1ByOrderByLastFulfilledDeliveryAtAsc();
+
+    Optional<Courier> findByPendingDeliveries_id(UUID deliveryId);
 }

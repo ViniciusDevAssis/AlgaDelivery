d3243e9ced32463bfe425e9354df4afa4ee9f82e
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2026-02-12 22:09:42 -0300

    Padrões de resiliencia

--- Microservices/Delivery-Tracking/src/main/java/com/viniciusdevassis/algadelivery/delivery/tracking/infrastructure/http/client/CourierPayoutCalculationServiceHttpImpl.java
+++ Microservices/Delivery-Tracking/src/main/java/com/viniciusdevassis/algadelivery/delivery/tracking/infrastructure/http/client/CourierPayoutCalculationServiceHttpImpl.java
@@ -1,8 +1,11 @@
 package com.viniciusdevassis.algadelivery.delivery.tracking.infrastructure.http.client;
 
 import com.viniciusdevassis.algadelivery.delivery.tracking.domain.service.CourierPayoutCalculationService;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.math.BigDecimal;
 
@@ -15,9 +18,14 @@ implements CourierPayoutCalculationService {
 
     @Override
     public BigDecimal calculatePayout(Double distanceInKm) {
-        CourierPayoutResultModel courierPayoutResultModel = courierAPIClient.payoutCalculation(
-                new CourierPayoutCalculationInput(distanceInKm)
-        );
-        return courierPayoutResultModel.getPayoutFee();
+        try {
+            var courierPayoutResultModel = courierAPIClient.payoutCalculation(
+                    new CourierPayoutCalculationInput(distanceInKm));
+            return courierPayoutResultModel.getPayoutFee();
+        } catch (ResourceAccessException e) {
+            throw new GatewayTimeoutException(e);
+        } catch (HttpServerErrorException | CallNotPermittedException | IllegalArgumentException e) {
+            throw new BadGatewayException(e);
+        }
     }
 }
